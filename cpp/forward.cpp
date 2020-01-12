23904af0c253c9cd3c9899033c4762a1101a775e
Bob Carpenter <carp@alias-i.com>
2020-01-12 15:37:55 -0500

    mixed examples in text and code

--- cpp/forward.cpp
+++ cpp/forward.cpp
@@ -12,4 +12,15 @@ int main() {
   dual<double> g = dual<double>(2) * dual<double>(3);
   dual<double> h = 2 * dual<double>(3);
   dual<double> i = dual<double>(2) * 3;
+
+  dual<dual<double>> aa(dual<double>(2.1, 1), dual<double>(0, 0));
+  dual<dual<double>> bb(dual<double>(1.5, 0), dual<double>(1, 0));
+  dual<dual<double>> cc(dual<double>(-0.3, 0), dual<double>(0, 0));
+
+  std::cout << "a = " << aa << std::endl;
+  std::cout << "b = " << bb << std::endl;
+  std::cout << "c = " << cc << std::endl;
+  std::cout << "b * c = " << (bb * cc) << std::endl;
+  std::cout << "exp(b * c) = " << exp(bb * cc) << std::endl;
+  std::cout << "a * exp(b * c) = " << (aa * exp(bb * cc)) << std::endl;
 }
--- cpp/forward.hpp
+++ cpp/forward.hpp
@@ -8,6 +8,8 @@ class dual {
       : val_(val), tan_(tan) { }
   const T& val() const { return val_; }
   const T& tan() const { return tan_; }
+  //   friend template <typename T>
+  // std::ostream& operator<<(std::ostream& o, const dual<T>& y) const;
 };
 }
 
@@ -33,10 +35,6 @@ inline dual<T> operator*(const dual<T>& x1, const dual<T>& x2) {
   return dual(x1.val() * x2.val(),
               x1.tan() * x2.val() + x1.val() * x2.tan());
 }
-}
-
-#include <type_traits>
-namespace autodiff {
 template <typename T, typename U,
           typename = std::enable_if_t<std::is_arithmetic_v<U>>>
 inline dual<T> operator*(const dual<T>& x1, const U& c2) {
@@ -46,5 +44,34 @@ template <typename T, typename U,
           typename = std::enable_if_t<std::is_arithmetic_v<U>>>
 inline dual<T> operator*(const U& c1, const dual<T>& x2) {
   return dual(c1 * x2.val(), c1 * x2.tan());
+}
+
+template <typename T>
+inline dual<T> operator+(const dual<T>& x1, const dual<T>& x2) {
+  return dual(x1.val() + x2.val(), x1.tan() + x2.tan());
+}
+template <typename T, typename U,
+          typename = std::enable_if_t<std::is_arithmetic_v<U>>>
+inline dual<T> operator+(const dual<T>& x1, const U& c2) {
+  return dual(x1.val() + c2, x1.tan());
+}
+template <typename T, typename U,
+          typename = std::enable_if_t<std::is_arithmetic_v<U>>>
+inline dual<T> operator+(const U& c1, const dual<T>& x2) {
+  return dual(c1 + x2.val(), x2.tan());
+}
+
+
+
+
+}
+
+
+#include <iostream>
+namespace autodiff {
+template <typename T>
+std::ostream& operator<<(std::ostream& o, const dual<T>& y) {
+  o << "<" << y.val() << ", " << y.tan() << ">";
+  return o;
 }
 }
