f7479701e5d291b668c05f4bdb0ce870eb5ce3a9
Bob Carpenter <carp@alias-i.com>
2020-01-13 18:08:18 -0500

    preface fill plus biblio

--- cpp/functional.hpp
+++ cpp/functional.hpp
@@ -21,8 +21,22 @@ void gradient(const F& f, const Eigen::VectorXd& x,
 
 template <typename F>
 void vector_gradient_product(const F& f, const Eigen::VectorXd& x,
-                             const Eigen::VectorXd v, double& fx, double& x_gradv) {
-
+                             const Eigen::VectorXd v, double& fx,
+                             double& x_gradv) {
 }
 
+Eigen::MatrixXd hessian(const F& f, const Eigen::VectorXd& x) {
+  int N = x.size();
+  Eigen::MatrixXd H(N, N);
+  for (int m = 0; m < N; ++m) {
+    // calculate m-th row of Hessian
+    Eigen::Matrix<dual<adj>, -1, 1> xa(N);
+    for (int n = 0; n < N; ++n)
+      xa(n) = { x(n), m == n };  // d.x(n)/d.x(m) = 1 iff m == n
+    auto y = f(xa);
+    std::vector<double> adjoints = chain(y);
+    for (int n = 0; n < N; ++n)
+      H(m, n) = adjoints[xa(n).tan().idx()];  // = bar(dot(x[n]))
+  }
+  return H;
 }
